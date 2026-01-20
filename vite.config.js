d5cffa4ae3dc1069c3d463d30468aebfb5b0786a
john3f589469-max <john3f589469@uju.com.ng>
2026-01-20 17:24:27 +0300

    Add Vite configuration for React project

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/almet-dating-mini/',
+  server: {
+    port: 5173,
+    strictPort: false
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  }
+})
